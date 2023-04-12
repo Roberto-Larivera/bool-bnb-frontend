4a7b935d5ecfd8adcd4f630eb3dc3ec37c68dcdc
“Francesca <dascanio.francesca@gmail.com>
2023-04-12 12:37:40 +0200

    preparazione card semplice

--- src/components/Main/Card.vue
+++
@@ -1,1 +0,0 @@
-<!-- Per eventuali componenti in main -->
--- src/pages/ApartmentIndex.vue
+++ src/pages/ApartmentIndex.vue
@@ -1,9 +1,15 @@
 <script>
+
+import AppCard from '../components/Main/AppCard.vue';
+
 export default {
   name: 'ApartmentIndex',
+  components:  {
+    AppCard
+  },
   data(){
     return{
-
+        apartment: []
     }
   }
   
@@ -11,15 +17,25 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-        <div class="col">
-           <h1>
-            ApartmentIndex
-           </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="py-3">
+                    Esplora gli appartamenti
+                </h1>
+
+                <h2>
+                    Ricerca avanzata
+                </h2>
+            </div>
         </div>
     </div>
-  </div>
+
+    <div class="container">
+        <div class="row g-3">
+            <AppCard :apartment="apartment" />
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
