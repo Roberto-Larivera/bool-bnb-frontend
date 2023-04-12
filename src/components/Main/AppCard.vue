0df73303ed0e72647a9c56bf8bcb090680cc64de
“Francesca <dascanio.francesca@gmail.com>
2023-04-12 13:08:33 +0200

    prova apartmentIndex cards

--- src/components/Main/AppCard.vue
+++ src/components/Main/AppCard.vue
@@ -40,6 +40,162 @@ export default {
         </div>
     </div>
 
+    <div class="card">
+        <img src="https://picsum.photos/200/200" class="card-img" alt="img-card">
+        <div class="card-body">
+            <h5 class="card-title">
+                Appartamento luminoso
+            </h5>
+            <div class="card-text">
+                <div>
+                    Via Roma, 2 - Roma, Italia
+                </div>
+                <div>
+                    140 &euro; notte
+                </div>
+                <div>
+                    200 mq
+                </div>
+            </div>
+            <!-- Da inserire rotta / nel caso preferiamo centrale text-center -->
+            <div>
+                <a href="#" class="my-btn">
+                    Dettagli
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="https://picsum.photos/200/200" class="card-img" alt="img-card">
+        <div class="card-body">
+            <h5 class="card-title">
+                Appartamento luminoso
+            </h5>
+            <div class="card-text">
+                <div>
+                    Via Roma, 2 - Roma, Italia
+                </div>
+                <div>
+                    140 &euro; notte
+                </div>
+                <div>
+                    200 mq
+                </div>
+            </div>
+            <!-- Da inserire rotta / nel caso preferiamo centrale text-center -->
+            <div>
+                <a href="#" class="my-btn">
+                    Dettagli
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="https://picsum.photos/200/200" class="card-img" alt="img-card">
+        <div class="card-body">
+            <h5 class="card-title">
+                Appartamento luminoso
+            </h5>
+            <div class="card-text">
+                <div>
+                    Via Roma, 2 - Roma, Italia
+                </div>
+                <div>
+                    140 &euro; notte
+                </div>
+                <div>
+                    200 mq
+                </div>
+            </div>
+            <!-- Da inserire rotta / nel caso preferiamo centrale text-center -->
+            <div>
+                <a href="#" class="my-btn">
+                    Dettagli
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="https://picsum.photos/200/200" class="card-img" alt="img-card">
+        <div class="card-body">
+            <h5 class="card-title">
+                Appartamento luminoso
+            </h5>
+            <div class="card-text">
+                <div>
+                    Via Roma, 2 - Roma, Italia
+                </div>
+                <div>
+                    140 &euro; notte
+                </div>
+                <div>
+                    200 mq
+                </div>
+            </div>
+            <!-- Da inserire rotta / nel caso preferiamo centrale text-center -->
+            <div>
+                <a href="#" class="my-btn">
+                    Dettagli
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Prova con carosello -->
+    <!-- <div class="card">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100 card-img" src="https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=800" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100 card-img" src="https://images.pexels.com/photos/144352/pexels-photo-144352.jpeg?auto=compress&cs=tinysrgb&w=800" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100 card-img" src="https://images.pexels.com/photos/301489/pexels-photo-301489.jpeg?auto=compress&cs=tinysrgb&w=800" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <div class="card-body">
+            <h5 class="card-title">
+                Appartamento luminoso
+            </h5>
+            <div class="card-text">
+                <div>
+                    Via Roma, 2 - Roma, Italia
+                </div>
+                <div>
+                    140 &euro; notte
+                </div>
+                <div>
+                    200 mq
+                </div>
+            </div>
+            <div>
+                <a href="#" class="my-btn">
+                    Dettagli
+                </a>
+            </div>
+        </div>
+    </div> -->
+
 </template>
 
 <style lang="scss" scoped>
--- src/pages/ApartmentIndex.vue
+++ src/pages/ApartmentIndex.vue
@@ -32,8 +32,10 @@ export default {
     </div>
 
     <div class="container">
-        <div class="row g-3">
-            <AppCard :apartment="apartment" />
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-3">
+            <div class="col">
+                <AppCard :apartment="apartment" />
+            </div>
         </div>
     </div>
 </template>
