116b805a1a6c6d636b9c6a72d1801553408283b9
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-04-13 11:20:23 +0200

    Sezione riepilogo prenotazione

--- src/pages/ApartmentShow.vue
+++ src/pages/ApartmentShow.vue
@@ -23,7 +23,6 @@ export default {
 
         <div>
           <span>
-            <font-awesome-icon icon="fa-solid fa-location-dot" />
             <a href="#">Bolzano, Trentino-Alto Adige/Südtirol, Italia</a>
           </span>
         </div>
@@ -34,7 +33,7 @@ export default {
           <br>
           <br>
           <br>
-          Carosello di immagini
+          !! Immagini da aggiungere !! 
           <br>
           <br>
           <br>
@@ -45,86 +44,153 @@ export default {
       </div>
 
       <div class="row">
-        <!-- Descrizione e host -->
-        <div class="col">
 
-          <div>
-            <h3>Minicase - Host: Sabina Angela</h3>
+        <div class="col-8">
+
+          <!-- Descrizione e host -->
+          <div class="row">
+            <div class="col">
+              <div>
+                <h3>Minicase - Host: Sabina Angela</h3>
+
+                <div>
+                  <span>4 ospiti · </span>
+                  <span>1 camera da letto · </span>
+                  <span>1 letto · </span>
+                  <span>1 bagno </span>
+                </div>
+
+              </div>
+
+              <div>
+                <img src="" alt="">
+              </div>
+
+            </div>
+          </div>
+          
+          <hr>
+
+          <!-- Caratteristiche in rilievo -->
+          <div class="col">
 
             <div>
-              <span>4 ospiti · </span>
-              <span>1 camera da letto · </span>
-              <span>1 letto · </span>
-              <span>1 bagno </span>
+              <h6>Sabina Angela è un Superhost.</h6>
+              <p>I Superhost sono host esperti, con valutazioni alte, dediti a offrire ai propri ospiti un soggiorno straordinario.</p>
+            </div>
+
+            <div>
+              <h6>Ottima esperienza di check-in</h6>
+              <p>Il 95% degli ospiti ha valutato con 5 stelle la procedura di check-in.</p>
+            </div>
+
+            <div>
+              <h6>Cancellazione gratuita entro le ore 12:00 del giorno 13 apr.</h6>
             </div>
 
           </div>
 
-          <div>
-            <img src="" alt="">
+          <hr>
+
+          <!-- Protezione aircover -->
+
+          <div class="col">
+            <p>Ogni prenotazione include una protezione gratuita in caso di cancellazione da parte dell'host, di inesattezze dell'annuncio e di altri problemi come le difficoltà in fase di check-in.</p>
+            <a href="#">Per saperne di più</a>
           </div>
 
+          <hr>
+
+          <!-- Descrizione dell'appartamento -->
+          <div class="col">
+            <p>The Mirror Houses are a pair of vacation homes, set in the marvellous surroundings of the South Tyrolean Dolomites, amidst a beautiful scenery of apple trees, just outside the city of Bolzano. The Mirror houses offer a unique chance to spend a beautiful vacation surrounded by contemporary architecture of the highest standards and the most astonishing Landscape and beauty nature has to offer.</p>
+            <a href="#">Mostra altro ></a>
+          </div>
+
+          <hr>
+
+          <!-- Sezione con le immagini classificate per oggetto rappresentato -->
+          <div class="col">
+            <h5>Cosa troverai</h5>
+            <div>
+              <br>
+              ...Elenco servizi aggiuntivi...
+              <br>
+              <br>
+            </div>
+          </div>
+
+          <!-- Servizi inclusi -->
+
+          <!-- Calendario con disponibilità -->
+
         </div>
-      </div>
-      
-      <hr>
+    
 
-      <!-- Caratteristiche in rilievo -->
-      <div class="col">
+        <!-- Riepilogo prenotazione e costi (sezione sul lato destro) -->
+        <div class="col-4">
+          <div class="booking-review shadow-lg p-4">
 
-        <div>
-          <h6>Sabina Angela è un Superhost.</h6>
-          <p>I Superhost sono host esperti, con valutazioni alte, dediti a offrire ai propri ospiti un soggiorno straordinario.</p>
+            <div class="mb-3">
+              <span class="fw-bold fs-5">220 € </span>
+              <span>notte</span>
+            </div>
+
+            <div>
+              <div class="mb-3">
+                  <div class="data">
+                    <label for="exampleFormControlInput1" class="check-in">Check-in</label>
+                    <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="">
+                  </div>
+                  <div class="data">
+                    <label for="exampleFormControlInput1" class="check-out">Check-out</label>
+                    <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="">
+                  </div>
+                </div>
+            </div>
+
+            <div class="mb-3">
+              <div>
+                <label for="exampleFormControlInput1" class="check-in">Ospiti</label>
+                <select class="form-select" id="exampleFormControlInput1" placeholder="">
+                  <option selected>1</option>
+                  <option value="1">2</option>
+                  <option value="2">3</option>
+                  <option value="3">4</option>
+                </select>
+              </div>
+            </div>
+
+            <button class="btn mb-2">
+              Prenota
+            </button>
+
+            <div class="mb-3">
+              <p class="grey-text text-center">Non riceverai alcun addebito in questa fase</p>
+            </div>
+
+            <div class="mb-3 d-flex justify-content-between">
+              <span class="text-decoration-underline">220 € x 5 notti</span>
+              <span>1.100 €</span>
+            </div>
+
+            <div class="mb-3 d-flex justify-content-between">
+              <span class="text-decoration-underline">Costi del servizio Airbnb</span>
+              <span>189 €</span>
+            </div>
+
+            <hr>
+
+            <div class="mb-3 d-flex justify-content-between">
+              <span class="fw-bold">Totale</span>
+              <span class="fw-bold">1289 €</span>
+            </div>
+
+          </div>
+          
         </div>
 
-        <div>
-          <h6>Ottima esperienza di check-in</h6>
-          <p>Il 95% degli ospiti ha valutato con 5 stelle la procedura di check-in.</p>
-        </div>
-
-        <div>
-          <h6>Cancellazione gratuita entro le ore 12:00 del giorno 13 apr.</h6>
-        </div>
-
       </div>
-
-      <hr>
-
-      <!-- Protezione aircover -->
-
-      <div class="col">
-        <p>Ogni prenotazione include una protezione gratuita in caso di cancellazione da parte dell'host, di inesattezze dell'annuncio e di altri problemi come le difficoltà in fase di check-in.</p>
-        <a href="#">Per saperne di più</a>
-      </div>
-
-      <hr>
-
-      <!-- Descrizione dell'appartamento -->
-      <div class="col">
-        <p>The Mirror Houses are a pair of vacation homes, set in the marvellous surroundings of the South Tyrolean Dolomites, amidst a beautiful scenery of apple trees, just outside the city of Bolzano. The Mirror houses offer a unique chance to spend a beautiful vacation surrounded by contemporary architecture of the highest standards and the most astonishing Landscape and beauty nature has to offer.</p>
-        <a href="#">Mostra altro ></a>
-      </div>
-
-      <hr>
-
-      <!-- Sezione con le immagini classificate per oggetto rappresentato -->
-      <div class="col">
-        <h5>Cosa troverai</h5>
-        <div>
-          <br>
-          ...Elenco servizi aggiuntivi...
-          <br>
-          <br>
-        </div>
-      </div>
-
-      <!-- Servizi inclusi -->
-
-      <!-- Calendario con disponibilità -->
-
-      <!-- Riepilogo prenotazione e costi (sezione sul lato destro) -->
-
-      <!-- Recensioni ??? -->
 
       <!-- Localizzazione dell'appartamento -->
 
@@ -136,5 +202,21 @@ export default {
 
 
 <style lang="scss" scoped>
+
+.booking-review {
+  border-radius: 20px;
+  background-color: $color_light;
+  min-height: 200px;
+}
+
+.btn {
+  width: 100%;
+  background-color: $color_primary;
+  color: $color_light;
+}
+
+.grey-text{
+  color: $color_gray;
+}
 
 </style>
