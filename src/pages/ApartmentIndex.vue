ba9ebaeb3cf254816750d1188cb4fb1dc3a9e74a
“Francesca <dascanio.francesca@gmail.com>
2023-04-13 11:51:41 +0200

    ok index

--- src/pages/ApartmentIndex.vue
+++ src/pages/ApartmentIndex.vue
@@ -30,27 +30,81 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col">
-                <div>
-                    <!-- Mobile -->
-                    <button type="button" class="search my-btn rounded-pill my-3 me-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <div class="mt-2">
+                    <!-- Ricerca - Mobile -->
+                    <button type="button" class="search my-btn rounded-pill my-3 p-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         Ricerca appartamenti
                     </button>
 
-                    <!-- Tablet / Desktop -->
-                    <div class="form-container rounded-pill p-3 shadow p-3 bg-body-tertiary rounded">
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                        Ricerca appartamenti
+                                    </h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="" class="form-container-small">
+                                        <div class="mb-3">
+                                            <label for="place" class="form-label">
+                                                Dove
+                                            </label>
+                                            <input type="text" class="form-control" id="place">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="check-in" class="form-label">
+                                                Check-in
+                                            </label>
+                                            <input type="date" class="form-control" id="check-in">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="check-out" class="form-label">
+                                                Check-out
+                                            </label>
+                                            <input type="date" class="form-control" id="check-out">
+                                        </div>
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option selected>Seleziona nr. ospiti</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                        </select>
+                                        <button type="submit" class="my-submit rounded">
+                                            Vai
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
+                    <!-- Ricerca - Tablet / Desktop -->
+                    <div class="form-container-large rounded-pill p-2 shadow bg-body-tertiary rounded">
                         <form action="" class="d-flex justify-content-between align-items-center">
                             <span>
-                                <span class="mb-3">
-                                <input type="text" id="place" placeholder="Cerca destinazione">
+                                <span class="form-floating mb-3">
+                                    <label for="place">
+                                        Dove
+                                    </label>
+                                    <input type="text" id="place">
                                 </span>
-                                <span class="mb-3">
+                                <span class="form-floating mb-3">
                                     <input type="date" id="check-in" placeholder="Da quando">
                                 </span>
-                                <span class="mb-3">
+                                <span class="form-floating mb-3">
                                     <input type="date" id="check-out" placeholder="A quando">
                                 </span>
-                                <span class="mb-3">
-                                    <input type="number" id="guests" placeholder="Nr. ospiti" max="20">
+                                <span class="form-floating mb-3">
+                                    <label for="place">
+                                        Ospiti
+                                    </label>
+                                    <input type="number" id="guests" max="20">
                                 </span>
                             </span>
                             <button type="submit" class="my-submit rounded-pill px-3">
@@ -58,85 +112,77 @@ export default {
                             </button>
                         </form>
                     </div>
-                    <button type="button" class="filter my-btn m-3 rounded-pill">
+
+                    <!-- Bottone filtri avanzati -->
+                    <button type="button" class="filter my-btn ms-2 p-3 rounded-pill" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                         <font-awesome-icon icon="fa-solid fa-sort" /> Filtri avanzati
                     </button>
 
-                </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                        Filtri avanzati
+                                    </h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label for="price" class="form-label">
+                                            Prezzo massimo
+                                        </label>
+                                        <input type="number" class="form-control" id="price">
+                                    </div>
 
+                                    <div class="mb-3">
+                                        <label for="beds" class="form-label">
+                                            Nr. letti
+                                        </label>
+                                        <input type="text" class="form-control" id="beds" max="20">
+                                    </div>
+                        
+                                    <div class="mb-3">
+                                        Servizi 
+                                    </div>
+                                    <div class="form-check">
+                                        <div class="mb-1" v-for="service in services">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                            <label class="form-check-label" for="flexCheckDefault">
+                                                {{ service }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="my-btn rounded" data-bs-dismiss="modal">
+                                    Esci
+                                </button>
+                                <button type="button" class="my-submit rounded">
+                                    Aggiungi filtri
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Titolo pagina -->
     <div class="container">
         <div class="row">
-            <div class="col p-0">
-                <h1 class="mt-3">
+            <div class="col p-0 my-5">
+                <h1>
                     Tutti gli appartamenti BoolBnB
                 </h1>
             </div>
         </div>
     </div>
 
-
-                <!-- <div class="form-container d-flex justify-content-center my-3 py-3 shadow p-3 mb-5 bg-body-tertiary rounded">
-                    <form action="" class="col-10"> 
-                        <div class="mb-3">
-                            <input type="text" class="form-control" id="place" placeholder="Dove">
-                        </div>
-
-                        <div class="mb-3">
-                            <input type="date" class="form-control" id="check-in" placeholder="Check-in">
-                        </div>
-
-                        <div class="mb-3">
-                            <input type="date" class="form-control" id="check-out" placeholder="Check-out">
-                        </div>
-
-                        <div class="mb-3">
-                            <input type="number" class="form-control" id="guests" placeholder="Numero ospiti" max="20">
-                        </div>
-
-
-                        <div :class="moreFilters == false ? 'd-block' : 'd-none'">
-                            <button type="button" class="my-btn mb-3" @click="moreFilters = true">
-                                <font-awesome-icon icon="fa-solid fa-sort" /> Filtri avanzati
-                            </button>
-                        </div>
-
-                        <div :class="moreFilters == false ? 'd-none' : 'd-block'">
-                            <div class="mb-3">
-                                <label for="price" class="form-label">
-                                    Prezzo massimo
-                                </label>
-                                <input type="number" class="form-control" id="price">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="beds" class="form-label">
-                                    Nr. letti
-                                </label>
-                                <input type="text" class="form-control" id="beds" max="20">
-                            </div>
-                
-                            <div class="mb-3">
-                                Servizi 
-                            </div>
-                            <div class="form-check">
-                                <div class="mb-1" v-for="service in services">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        {{ service }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                
-                        <button type="submit" class="my-submit my-3">
-                            Cerca
-                        </button>
-                    </form>
-                </div> -->
-            </div>
-        </div>
-    </div>
-
+    <!-- Cards -->
     <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-sm-3 g-md-3 g-lg-3">
             <AppCard :apartment="apartment"/>
@@ -155,67 +201,38 @@ export default {
         border: 1px solid $color_gray;
     }
 
-// .form-container {
-//     border: 1px solid $color_gray;
-//     border-radius: 40px 999em 999em 40px;
 
-//     .my-btn {
-//         padding: 0.5rem;
-//         display: inline-block;
-//         background-color: $color_light_gray;
-//         color: $color_dark;
-//         text-decoration: none;
-//         border-radius: 3px;
-//         border: 1px solid $color_gray;
-//     }
+.modal-content {
 
+    border-radius: 0.5rem;
 
-//     .more-filters {
+    .modal-body {
+        border-radius: 0.5rem;
 
-//         display: none;
-//         .form-check-input {
-//         &:checked {
-//             background-color: $color_primary;
-//             border-color: $color_primary;
-//         }
-//     }
-//     }
-    
-    
-//     .my-submit {
-//         padding: 0.5rem;
-//         display: inline-block;
-//         background-color: $color_primary;
-//         color: $color_light;
-//         text-decoration: none;
-//         border-radius: 3px;
-//         border: 1px solid $color_primary;
+        .form-container-small {
 
-//     }
-// }
-
-
-// Responsive
-@media screen and (min-width: 0px) {
-    .search {
-        display: inline-block;
-    }
-    .form-container {
-        display: none;
+        .my-submit {
+            padding: 0.5rem;
+            display: inline-block;
+            background-color: $color_primary;
+            color: $color_light;
+            text-decoration: none;
+            border: 1px solid $color_primary;
+            margin-top: 2rem;
+            padding: 0.5rem 1rem;
+        }
+}
     }
 }
 
-@media screen and (min-width: 1200px) {
-    .search {
-        display: none;
-    }
-    .form-container {
-        display: inline-block;
-        width: 85%;
-        border: 1px solid $color_gray;
+.form-container-large {
+    width: 85%;
+    border: 1px solid $color_gray;
 
-        label {
-            display: block;
+    .form-floating > label {
+            padding: 0 0.5rem;
+            overflow: visible;
+            top: -2px;
         }
 
         input {
@@ -224,6 +241,7 @@ export default {
             border-left-style: hidden;
             border-bottom-style: hidden;
             background-color: $color_light;
+            margin-left: 4rem;
         }
 
         .no-outline:focus {
@@ -246,6 +264,67 @@ export default {
             border: 1px solid $color_primary;
 
         }
+
+}
+
+.modal-content {
+    background-color: $color_light;
+    border-radius: 0.5rem 0.5rem 0 0;
+    border: none;
+
+    .modal-body {
+        border-radius: 0.5rem 0.5rem 0 0;
+
+        .form-check-input {
+
+            &:checked {
+                background-color: $color_primary;
+            }
+        }
+    }
+
+}
+
+.modal-footer {
+        background-color: $color_light;
+
+        .my-btn {
+        padding: 0.5rem;
+        display: inline-block;
+        background-color: $color_light_gray;
+        color: $color_dark;
+        text-decoration: none;
+        border: 1px solid $color_gray;
+    }
+
+    .my-submit {
+            padding: 0.5rem;
+            display: inline-block;
+            background-color: $color_primary;
+            color: $color_light;
+            text-decoration: none;
+            border: 1px solid $color_primary;
+
+        }
+}
+
+
+// Responsive
+@media screen and (min-width: 0px) {
+    .search {
+        display: inline-block;
+    }
+    .form-container-large {
+        display: none;
+    }
+}
+
+@media screen and (min-width: 1200px) {
+    .search {
+        display: none;
+    }
+    .form-container-large {
+        display: inline-block;
 
     }
 
