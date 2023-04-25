d51f2bce87785c8747298e1dd3ca70b941e9a25a
Andrea Raguso <info@andrearaguso.com>
2023-04-25 20:41:54 +0200

    fix visible

--- src/components/Main/AppCard.vue
+++ src/components/Main/AppCard.vue
@@ -1,144 +1,136 @@
 <script>
-import { delay } from 'lodash';
+import { delay } from "lodash";
 
 export default {
-    name: 'AppCard',
-    props: {
-        apartment: Object,
-        caricamento: Boolean
-    },
-    data() {
-        return {
-            delay: false
-        }
-    },
-    methods: {
-    },
-    created() { 
- 
-        setTimeout(() => this.delay = true, 1000);
-
-    }
-}
-
+  name: "AppCard",
+  props: {
+    apartment: Object,
+    caricamento: Boolean,
+  },
+  data() {
+    return {
+      delay: false,
+    };
+  },
+  methods: {},
+  created() {
+    setTimeout(() => (this.delay = true), 1000);
+  },
+};
 </script>
 
-
 <template>
-    <template v-if="apartment.visible == 1">
-        <template v-if="delay">
-            <div class="col">
-                <router-link :to="{ name: 'apartments-show', params: { slug: apartment.slug }}" class="text-decoration-none">
-                    <div class="card position-relative">
-                        <font-awesome-icon v-if="apartment.sponsored" class="position-absolute my-icon-sponsor rounded-circle" :icon="['fas', 'rocket']" />
-                    <img :src="apartment.main_img" class="card-img" alt="img-card">
-                    <div class="card-body text-center text-lg-start">
-                        <h5 class="card-title">
-                            {{ apartment.title }}
-                        </h5>
-                        <div class="card-text">
-                            <div>
-                                {{ apartment.address }}
-                            </div>
-                            <div>
-                                {{ apartment.price }} &euro; notte
-                            </div>
-                            <div>
-                                {{ apartment.mq }} mq
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </router-link>
-                <!-- Da inserire rotta / nel caso preferiamo centrale text-center -->
+  <template v-if="delay">
+    <div class="col">
+      <router-link
+        :to="{ name: 'apartments-show', params: { slug: apartment.slug } }"
+        class="text-decoration-none"
+      >
+        <div class="card position-relative">
+          <font-awesome-icon
+            v-if="apartment.sponsored"
+            class="position-absolute my-icon-sponsor rounded-circle"
+            :icon="['fas', 'rocket']"
+          />
+          <img :src="apartment.main_img" class="card-img" alt="img-card" />
+          <div class="card-body text-center text-lg-start">
+            <h5 class="card-title">
+              {{ apartment.title }}
+            </h5>
+            <div class="card-text">
+              <div>
+                {{ apartment.address }}
+              </div>
+              <div>{{ apartment.price }} &euro; notte</div>
+              <div>{{ apartment.mq }} mq</div>
             </div>
-        </template>
+          </div>
+        </div>
+      </router-link>
+      <!-- Da inserire rotta / nel caso preferiamo centrale text-center -->
+    </div>
+  </template>
 
-        <template v-else>
-            <!-- placeholder -->
-            <!-- finchè carica placeholder, altrimenti card -->
-            <div class="col">
-                <div class="card" aria-hidden="true">
-                    <span class="card-img" style="background-color: lightgray;"></span>
-                    <div class="card-body text-center text-lg-start">
-                        <h5 class="card-title placeholder-glow">
-                            <span class="placeholder col-6"></span>
-                        </h5>
-                        <div class="card-text placeholder-glow">
-                            <span class="placeholder col-7"></span>
-                            <span class="placeholder col-4"></span>
-                            <span class="placeholder col-4"></span>
-                            <span class="placeholder col-6"></span>
-                            <span class="placeholder col-8"></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </template>
-    </template>
+  <template v-else>
+    <!-- placeholder -->
+    <!-- finchè carica placeholder, altrimenti card -->
+    <div class="col">
+      <div class="card" aria-hidden="true">
+        <span class="card-img" style="background-color: lightgray"></span>
+        <div class="card-body text-center text-lg-start">
+          <h5 class="card-title placeholder-glow">
+            <span class="placeholder col-6"></span>
+          </h5>
+          <div class="card-text placeholder-glow">
+            <span class="placeholder col-7"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-6"></span>
+            <span class="placeholder col-8"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
 </template>
 
 <style lang="scss" scoped>
 .card {
-    padding: 0;
-    border: none;
+  padding: 0;
+  border: none;
+  border-radius: 5px;
+  background-color: transparent;
+
+  .card-img {
+    max-width: 100%;
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
     border-radius: 5px;
-    background-color: transparent;
-
-    .card-img {
-        max-width: 100%;
-        aspect-ratio: 1 / 1;
-        object-fit: cover;
-        border-radius: 5px;
-    }
-    .my-icon-sponsor{
-        top: 10px;
-        right: 10px;
-        color: $color_primary;
-        padding: 10px;
-        background-color:$color_light ;
-
-    }
+  }
+  .my-icon-sponsor {
+    top: 10px;
+    right: 10px;
+    color: $color_primary;
+    padding: 10px;
+    background-color: $color_light;
+  }
 }
 
-
-
 .card-body {
-    padding: 1rem 0;
-    background-color: transparent;
+  padding: 1rem 0;
+  background-color: transparent;
 
-    .card-title {
-        color: $color_dark;
+  .card-title {
+    color: $color_dark;
+  }
+
+  .card-text {
+    color: $color_gray;
+    font-size: 0.8rem;
+    padding: 0.5rem 0;
+
+    div {
+      padding: 0.2rem 0;
     }
-
-    .card-text {
-        color: $color_gray;
-        font-size: 0.8rem;
-        padding: 0.5rem 0;
-
-        div {
-            padding: 0.2rem 0;
-        }
-    }
+  }
 }
 
 // Non sono convinta della larghezza del bottone
 .my-btn {
-    font-size: 0.8rem;
-    // width: 100%;
-    padding: 0.5rem;
-    display: inline-block;
-    background-color: $color_light_gray;
-    color: $color_dark;
-    text-decoration: none;
-    border-radius: 3px;
-    border: 1px solid $color_gray;
+  font-size: 0.8rem;
+  // width: 100%;
+  padding: 0.5rem;
+  display: inline-block;
+  background-color: $color_light_gray;
+  color: $color_dark;
+  text-decoration: none;
+  border-radius: 3px;
+  border: 1px solid $color_gray;
 
-    &:hover {
-        background-color: $color_primary;
-        color: $color_light;
-        border: none;
-    }
+  &:hover {
+    background-color: $color_primary;
+    color: $color_light;
+    border: none;
+  }
 }
 </style>
-  
